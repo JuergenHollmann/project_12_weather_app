49df8b1ea12183ad0a987f66e4450320f90a8772
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-25 22:32:18 +0200

    127 changes to commit

---
+++ project_12_weather_app/android/app/src/main/kotlin/com/example/project_12_weather_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_12_weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ project_12_weather_app/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WeatherApp(
+        Scaffold(
+          body: Container(
+            // child: AppBar(
+            //   title: const Text('Wetter-App'),
+            //   foregroundColor: Colors.white, // Schriftfarbe
+            //   backgroundColor:
+            //       const Color.fromARGB(255, 4, 85, 151), // dunkles Blau
+            //   shadowColor: Colors.black87, // funzt nicht
+            //   elevation: 20, // graue Zone unter der AppBar
+            // ),
+            child: const Text("Willkommen zur 2. Wetter-App",
+                style: TextStyle(color: Colors.blue, fontSize: 24)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp(Scaffold scaffold, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: AppBar(
+        title: const Text('Wetter-App'),
+        foregroundColor: Colors.white, // Schriftfarbe
+        backgroundColor: const Color.fromARGB(255, 4, 85, 151), // dunkles Blau
+        shadowColor: Colors.black87, // funzt nicht
+        elevation: 20, // graue Zone unter der AppBar
+      ),
+      //  const Text("Willkommenzur Wetter-App",
+      //   style: TextStyle(color: Colors.blue, fontSize: 24)),
+    );
+  }
+}
+
+
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Hello World!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
