7ab7523137a213b89f094e3e783d03f97a4445d4
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-26 20:37:23 +0200

    set city, temperature an weather

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(0, 170, 188, 255),
+        backgroundColor: Color.fromARGB(255, 17, 0, 209),
         body: SafeArea(
           child: WeatherApp(),
         ),
@@ -30,7 +30,8 @@ class WeatherApp extends StatelessWidget {
       child: Column(
         children: [
           AppBar(
-              title: const Text('Einfache Wetter-App'),
+              title: const Text("Einfache Wetter-App",
+                  style: TextStyle(fontSize: 28)),
               foregroundColor: Colors.white, // Schriftfarbe
               backgroundColor:
                   const Color.fromARGB(255, 4, 85, 151), // dunkles Blau
@@ -40,6 +41,34 @@ class WeatherApp extends StatelessWidget {
           const SizedBox(height: 40),
           const Text("Willkommen zur Wetter-App",
               style: TextStyle(color: Colors.blue, fontSize: 24)),
+          const SizedBox(height: 0),
+          const Divider(thickness: 2, height: 60, indent: 20, endIndent: 20),
+          const Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Stadt:",
+                    style: TextStyle(color: Colors.amber, fontSize: 20)),
+                SizedBox(height: 10),
+                Text("Schwäbisch Gmünd",
+                    style: TextStyle(color: Colors.amber, fontSize: 26)),
+                Divider(thickness: 2, height: 60, indent: 20, endIndent: 20),
+
+                Text("Temperatur:",
+                    style: TextStyle(color: Colors.amber, fontSize: 20)),
+                SizedBox(height: 10),
+                Text("28 °C",
+                    style: TextStyle(color: Colors.amber, fontSize: 36)),
+                Divider(thickness: 2, height: 60, indent: 20, endIndent: 20),
+
+                Text("Wetter:",
+                    style: TextStyle(color: Colors.amber, fontSize: 20)),
+                SizedBox(height: 10),
+                Text("leicht bewölkt",
+                    style: TextStyle(color: Colors.amber, fontSize: 26)),
+                Divider(thickness: 2, height: 60, indent: 20, endIndent: 20),
+
+              ]),
         ],
       ),
     );
